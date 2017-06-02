3ff272d4cce9b6c9d1cdbf4f16aa77f888b7fe87
kazuooooo <matsumotokazuya7@gmail.com>
2017-06-02 11:23:41 +0900

    v0.2.0

--- spec/twitter_actions_spec.rb
+++ spec/twitter_actions_spec.rb
@@ -1,5 +1,18 @@
 require "spec_helper"
 
 describe TwitterActions do
+  it "foo" do
+    TwitterActions.configure do |config|
+      config.consumer_key = "zk8E71XFeIKrb6YJFd5nFDXYs"
+      config.consumer_secret = "uWM54bHDRBCwZfmB3JETc9Vr0I7JGspDdZ4idArcpu7Vuun12w"
+      config.access_token = "869958377069101056-q1cXdARnZeWMHAMeFPVcHIiV8Ct03zQ"
+      config.access_token_secret = "iMECTaZgDjF8IDzhFaziX1inA8ACSLRo5gNS1Za9psbiT"
+    end
+    A = Class.new do
+      include TwitterActions::Tweet
+    end
+    binding.pry
 
+    1+1
+  end
 end
